3e91ca7584acab9b0d900f9e7f1df09db16700b5
JoaquinMarcoss <joaquinmvlr03@gmail.com>
2024-05-13 17:05:41 -0300

    Update Login

--- final/backend/main.go
+++ final/backend/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"backend/app"
 
+	"backend/clients"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	clients.StartDB()
+
 	engine := gin.New()
 	app.MapRoutes(engine)
 	engine.Run(":8080")
